6aa3aae8503c617a38cafdf4eef523c6295e03a5
dima <dmsiods0@gmail.com>
2024-03-01 09:32:39 +0300

    27: ListView 2

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -21,6 +21,22 @@ class HomeNews(ListView):
         return News.objects.filter(is_published=True)
 
 
+class NewsByCategory(ListView):
+    model = News
+    template_name = 'news/category.html'
+    context_object_name = 'news'
+    allow_empty = False
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = Category.objects.get(pk=self.kwargs['category_id'])
+
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(is_published=True, category_id=self.kwargs['category_id'])
+
+
 # def index(request):
 #     news = News.objects.order_by('-created_at')
 #
@@ -40,11 +56,11 @@ def test(request):
     return render(request, 'news/test.html', {})
 
 
-def get_category(request, category_id):
-    news = News.objects.filter(category_id=category_id)
-    category = Category.objects.get(pk=category_id)
-
-    return render(request, 'news/category.html', {'news': news, 'category': category})
+# def get_category(request,category_id):
+# #     news = News.objects.filter(category_id=category_id)
+# #     category = Category.objects.get(pk=category_id)
+# #
+# #     return render(requ est, 'news/category.html', {'news': news, 'category': category})
 
 
 def view_news(request, news_id):
